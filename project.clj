84c21544e38a797b92f65b0e54a068d6db3e06b0
Ravindra R. Jaju <jaju@msync.org>
2019-08-25 08:48:58 +0530

    Updated dev info in project.clj. Also, personal-pref formatting

--- project.clj
+++ project.clj
@@ -1,26 +1,43 @@
 (defproject wit/duckling "0.4.25-SNAPSHOT"
+
   :description "Date & Number parser"
-  :license {:url "https://github.com/jaju/duckling-clj"
+
+  :license {:url      "https://github.com/jaju/duckling-clj"
             :comments "see LICENSE"}
+
   :url "https://github.com/jaju/duckling-clj"
+
   :plugins [[lein-midje "3.2.1"]]
+
   :repl-options {:init-ns duckling.core}
+
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [org.clojure/tools.nrepl "0.2.13"]
                  [org.clojure/tools.logging "0.5.0"]
                  [clj-time "0.15.2"]
                  [prismatic/plumbing "0.5.5"]]
+
   :deploy-repositories [["clojars" {:creds :gpg}]]
-  :profiles {:dev {:dependencies [[org.clojure/tools.trace "0.7.10"]
-                                  [midje "1.9.9"]
-                                  [cheshire "5.9.0"]]}
+
+  :profiles {:dev     {:dependencies [[org.clojure/tools.trace "0.7.10"]
+                                      [midje "1.9.9"]
+                                      [cheshire "5.9.0"]]}
              :uberjar {:aot [duckling.core]}}
-  :test-selectors {:default (complement :benchmark)
+
+  :test-selectors {:default   (complement :benchmark)
                    :benchmark :benchmark}
+
   :scm {:name "git"
-        :url "https://github.com/wit-ai/duckling"}
-  :pom-addition [:developers [:developer
-                               [:name "Wit.ai"]
-                               [:url "https://wit.ai"]
-                               [:email "contact@wit.ai"]
-                               [:timezone "-7"]]])
+        :url  "https://github.com/jaju/duckling-clj"}
+
+  :pom-addition [:developers
+                 [:developer
+                  [:name "Wit.ai"]
+                  [:url "https://wit.ai"]
+                  [:email "contact@wit.ai"]
+                  [:timezone "-7"]]
+                 [:developer
+                  [:name "Ravindra Jaju"]
+                  [:url "https://msync.org/"]
+                  [:email "dev@msync.org"]
+                  [:timezone "+5:30"]]])
